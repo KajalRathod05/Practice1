a2c30d50cf3887451d404b452a2d0b76e81b8ee2
KajalRathod05 <kajalrathod0576@gmail.com>
2024-09-20 06:50:22 +0530

    My SpringBoot Project

---
+++ Spring_Demo_Project/src/main/java/com/example/demo/SpringDemoProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoProjectApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Spring boot project starts...");
+		SpringApplication.run(SpringDemoProjectApplication.class, args);
+	}
+
+}
