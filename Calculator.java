c299f9f7625833e871736d68b8dc2b696176a6d1
Kajal Rathod <120319504+KajalRathod05@users.noreply.github.com>
2026-09-21 17:47:13 +0530

    Add multiplication method to Calculator
    mutiplication operation added

--- Calculator.java
+++ Calculator.java
@@ -4,5 +4,10 @@ public class Calculator {
         int a = 10;
         int b = 20;
         System.out.println("sum of a and b is: " + (a + b));
+        System.out.println("Multiplication of a and b is: " + multiply(a , b));
+    }
+
+    public int multiply(int a,int b){
+        return a*b;
     }
 }
