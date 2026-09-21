0dc932ec97c1a9889a0284085b555448ea640eba
KajalRathod05 <kajalrathod0576@gmail.com>
2026-09-21 17:28:37 +0530

    addition operation

--- Student.java
+++ Student.java
@@ -2,5 +2,9 @@ public class Student{
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int a=10;
+        int b=20;
+        System.out.println("sum of a and b is: " + (a+b));
     }
 }
